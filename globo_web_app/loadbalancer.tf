9704946161a13021caeea26b375c8281b5da7c7a
sabur7 <sabur7@gmail.com>
2023-01-16 13:30:09 -0500

    added load balancer config

--- globo_web_app/loadbalancer.tf
+++ globo_web_app/loadbalancer.tf
@@ -1,24 +1,50 @@
-# aws_lb
-# resource "aws_lb" "nginx" {
-#   name               = "globo_web_alb"
-#   internal           = false
-#   load_balancer_type = "application"
-#   security_groups    = [aws_security_group.alb_sg.id]
-#   subnets            = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+# aws_lb (AWS Application Load Balancer)
+resource "aws_lb" "nginx" {
+  name               = "globo_web_alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
 
-#   enable_deletion_protection = false
+  enable_deletion_protection = false
 
 
-#   tags = local.common_tags
-# }
-# # aws_lb_target_group
-# resource "aws_lb_target_group" "test" {
-#   name     = "tf-example-lb-tg"
-#   port     = 80
-#   protocol = "HTTP"
-#   vpc_id   = aws_vpc.vpc.id
-# }
+  tags = local.common_tags
+}
+
+# aws_lb_target_group
+resource "aws_lb_target_group" "nginx" {
+  name     = "tf-example-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc.id
+  tags.    = local.common_tags
+}
 
 # aws_lb_listener
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.nginx.arn
+  port              = "80"
+  protocol          = "HTTP"  
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nginx.arn
+  }
+
+  tags.              = local.common_tags
+}
 
 # aws_lb_target_group_attachment
+resource "aws_lb_target_group_attachment" "test" {
+  target_group_arn = aws_lb_target_group.nginx.arn
+  target_id        = aws_instance.nginx1.id
+  port             = 80
+}
+
+# aws_lb_target_group_attachment
+resource "aws_lb_target_group_attachment" "test" {
+  target_group_arn = aws_lb_target_group.nginx.arn
+  target_id        = aws_instance.nginx2.id
+  port             = 80
+}
