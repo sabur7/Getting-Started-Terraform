b4e6a6f2ad5726ad4dc8541c3ff19c3481504303
sabur7 <sabur7@gmail.com>
2023-01-16 13:37:54 -0500

    fix typo in tags assignment

--- globo_web_app/loadbalancer.tf
+++ globo_web_app/loadbalancer.tf
@@ -18,7 +18,7 @@ resource "aws_lb_target_group" "nginx" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.vpc.id
-  tags.    = local.common_tags
+  tags     = local.common_tags
 }
 
 # aws_lb_listener
@@ -32,7 +32,7 @@ resource "aws_lb_listener" "front_end" {
     target_group_arn = aws_lb_target_group.nginx.arn
   }
 
-  tags.              = local.common_tags
+  tags               = local.common_tags
 }
 
 # aws_lb_target_group_attachment
